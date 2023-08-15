2554912015a4f44d2d023660be3f13f326f9227d
Mrfoxc121 <cfox0115@gmail.com>
2023-08-15 16:27:33 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
-import OurClasses from "@/scenes/ourClasses";
-import Benefits from "@/scenes/benefits";
-import ContactUs from "@/scenes/contactUs";
-import Footer from "@/scenes/footer";
+import OurClasses from "@/scenes/OurClasses";
+import Benefits from "@/scenes/Benefits";
+import ContactUs from "@/scenes/ContactUs";
+import Footer from "@/scenes/Footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
